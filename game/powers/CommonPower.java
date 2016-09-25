59cb567f13ed34b26cfc7843d10ad39a607855cd
BobrZlosyn <bobrzlosyn@gmail.com>
2016-09-26 01:33:03 +0200

    zalozeni

---
+++ game/powers/CommonPower.java
@@ -0,0 +1,7 @@
+package game.powers;
+
+/**
+ * Created by BobrZlosyn on 26.09.2016.
+ */
+public class CommonPower {
+}
---
+++ game/shields/CommonShield.java
@@ -0,0 +1,7 @@
+package game.shields;
+
+/**
+ * Created by BobrZlosyn on 26.09.2016.
+ */
+public class CommonShield {
+}
